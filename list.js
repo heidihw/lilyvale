e7fd10124176cc0d17b44a1a6f25827b51b16193
dylene5 <dylene5@uw.edu>
2024-05-05 15:34:59 -0700

    idk what happened

---
+++ list.js
@@ -0,0 +1,17 @@
+/**
+ * Name:    Heidi Wang
+ * Date:    2024 05 05 Sun
+ * Section: CSE 154 AG
+ *
+ * list.js interactions specific to list.html
+ */
+
+'use strict';
+(function() {
+  window.addEventListener('load', init);
+
+  function init() {
+
+  }
+
+})();
