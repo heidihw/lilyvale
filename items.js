99bcda083211c5f4884dcae428bd0ed009cc8223
Heidi Wang <heidiwang123@gmail.com>
2024-05-05 22:17:22 -0700

    items linters

--- items.js
+++ items.js
@@ -3,20 +3,36 @@
  * Date:    2024 05 05 Sun
  * Section: CSE 154 AG
  *
- * items.js interactions specific to items.html
- * pt.1-1
+ * Manages view toggle and filtering for the page showing all items.
+ *
  */
 
 'use strict';
 (function() {
   window.addEventListener('load', init);
 
+  /**
+   * Initializes the filters.
+   * Toggles between grid and list view.
+   */
   function init() {
     document.getElementById('view').addEventListener('change', () => {
       const items = document.getElementById('items');
       items.classList.toggle('list-view');
       console.log(items.classList);
     });
+
+    let filters = document.querySelectorAll('input')
+    for (let i = 0; i < filters.length; i++) {
+      filters[i].addEventListener('change', filter);
+    }
+  }
+
+  /**
+   * Functionality for the filters.
+   */
+  function filter() {
+    console.log('cry');
   }
 
 })();
