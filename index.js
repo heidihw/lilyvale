b854e1bde62bbf388e18681aa94f16d7b3d6c51e
Heidi Wang <heidiwang123@gmail.com>
2024-05-07 18:46:18 -0700

    combined all files

--- index.js
+++ index.js
@@ -1,20 +1,86 @@
 /**
+ * Name:    Daria Manguling, Heidi Wang
+ * Date:    2024 05 05 Sun
+ * Section: CSE 154 AG, Allison and Marina
+ *
+ * This is the index.html for all pages of the anime shopping site.
+ */
+
+/**
  * Name: Daria Manguling
  * Date: 5 May 2025
  * CSE 154 Section AG, Allison and Marina
  *
  * This is the product.js for specific interactivity on the product.html page.
  * Specifically, it allows a user to write reviews and post them on the product's page.
+ * Manages view toggle and filtering for the page showing all items.
  */
+
+/**
+ * Name:    Heidi Wang
+ * Date:    2024 05 05 Sun
+ * Section: CSE 154 AG
+ *
+ * items.js
+ * Manages view toggle and filtering for the page showing all items.
+ */
+
 'use strict';
 (function() {
   window.addEventListener('load', init);
 
   /**
+   * Daria Manguling
    * Initializes button to be able to write a review.
+   */
+  /**
+   * Heidi Wang
+   * Initializes the filters.
+   * Toggles between grid and list view.
    */
   function init() {
     makeButton();
+
+    document.getElementById('view').addEventListener('change', () => {
+      const items = document.getElementById('items');
+      items.classList.toggle('list-view');
+    });
+
+    let filters = document.querySelectorAll('input');
+    for (let i = 0; i < filters.length; i++) {
+      filters[i].addEventListener('change', filter);
+    }
+  }
+
+  /**
+   * Heidi Wang
+   * Functionality for the filters.
+   */
+  function filter() {
+    let cleared = true;
+    let filters = document.querySelectorAll('input');
+    let selected = [];
+    for (let i = 0; i < filters.length; i++) {
+      if (filters[i].checked) {
+        selected.push(filters[i]);
+        cleared = false;
+      }
+    }
+    let items = document.querySelectorAll('article');
+    if (cleared) {
+      for (let i = 0; i < items.length; i++) {
+        items[i].style.display = 'unset';
+      }
+    } else {
+      for (let i = 0; i < items.length; i++) {
+        items[i].style.display = 'none';
+        for (let j = 0; j < selected.length; j++) {
+          if (items[i].classList.contains(selected[j].id)) {
+            items[i].style.display = 'unset';
+          }
+        }
+      }
+    }
   }
 
   /**
